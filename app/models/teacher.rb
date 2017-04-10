ce2624f2aea83b820f5e98a9e0042cefd0f84e60
liuhui <liuhuijisuanji@gmail.com>
2017-04-10 15:57:15 +0800

    add dependent for teacher's model

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,5 +2,5 @@ class Teacher < ApplicationRecord
   has_many :teacher_relationships
   has_many :students, through: :teacher_relationships, source: :student
 
-  has_many :cous
+  has_many :cous, :dependent => :destroy
 end
